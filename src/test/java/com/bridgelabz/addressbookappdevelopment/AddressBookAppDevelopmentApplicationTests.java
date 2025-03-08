9db25ba8d994e1dc16cd46691ba1dc40c84c16dd
TikeshKardate <tikesh.kardate@gmail.com>
2025-03-08 15:55:41 +0530

    [Tikesh] Add. Address Book App Setup added

---
+++ src/test/java/com/bridgelabz/addressbookappdevelopment/AddressBookAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbookappdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
